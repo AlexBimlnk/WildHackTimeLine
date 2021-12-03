241a981d5ac21f2c3cbe9c51e7690969482ffa09
Denis <d.igoschin.igoshin@gmail.com>
2021-12-03 23:38:59 +0300

    тест api №1

---
+++ WildHackWebApp/client-app/src/components/timeline.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="timeline">
+    <div class="container">
+      <div class="timeline">
+
+        <timeline-event v-for="i in 10" :key="i" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import timelineEvent from "./timelineEvent.vue"
+export default {
+  components: {
+    timelineEvent,
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.timeline {
+  height: 100vh !important;
+  overflow-y: scroll;
+}
+</style>
+
+
--- WildHackWebApp/client-app/src/views/Home.vue
+++ WildHackWebApp/client-app/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home-page">
     <h1>Clear template</h1>
-    <div class="container">
-      <news-card />
+    <div class="container-timeline">
+      <news-card
+        @click="openCardNews(content)"
+        v-for="content in $store.state.contents"
+        :key="content"
+        :cardContent="content"
+      />
     </div>
+    <timeline />
   </div>
 </template>
 
 <script>
 import newsCard from "../components/newsCard.vue";
+import timeline from "../components/timeline.vue"
 export default {
   components: {
     newsCard,
+    timeline
+  },
+  // data() {
+  //   return {};
+  // },
+  methods: {
+    openCardNews(item) {
+      this.$router.push(`/news/${item.title}`)
+    },
   },
 };
 </script>
-
