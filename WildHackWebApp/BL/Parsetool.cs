095c80f797028c220386e1b4c8d9a0a593ed69e5
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2021-12-03 22:42:29 +0300

    Переименовал Class.cs в Parsetool.cs и добавил пример работы парсера.

---
+++ WildHackWebApp/BL/Parsetool.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using WildHackWebApp.Models;
+
+namespace WildHackWebApp.BL
+{
+    public class Parsetool
+    {
+        /// <summary>
+        /// Возвращает новые статьи
+        /// </summary>
+        public static List<EcologyEvent> GetLastUpdates()
+        {
+            List<EcologyEvent> resultList = new List<EcologyEvent>();
+            string url = "https://poluostrov-kamchatka.ru/pknews/english/14/";
+            string cls = "article-info";
+
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(url);
+
+            var tags = doc.DocumentNode.SelectNodes($"//div[@class='{cls}']");
+
+            int i = 0;
+            foreach (HtmlNode node in tags)
+            {
+                EcologyEvent ecologyEvent = new EcologyEvent();
+                ecologyEvent.Id = i;
+                ecologyEvent.Title = node.SelectSingleNode("./p[1]").InnerText;
+                resultList.Add(ecologyEvent);
+                i++;
+            }
+
+            return resultList;
+        }
+    }
+}
