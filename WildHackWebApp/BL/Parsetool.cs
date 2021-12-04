df8bd8902fb98337591875ba4e144a008a69218b
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2021-12-04 18:00:53 +0300

    Поменял массивы строк на объекты PageLement

--- WildHackWebApp/BL/Parsetool.cs
+++ WildHackWebApp/BL/Parsetool.cs
@@ -10,23 +10,29 @@ namespace WildHackWebApp.BL
 {
     public class ParseTool
     {
-        private static Dictionary<SiteName, string[]> siteDict = new Dictionary<SiteName, string[]>
+        private static Dictionary<SiteName, PageElement> siteDict = new Dictionary<SiteName, PageElement>
         {
-            {SiteName.PoluostrovKamchatka, new string[] { "https://poluostrov-kamchatka.ru/pknews/english/",
-                                                          "//div[@class='article-info']",
-                                                          ".//h3",
-                                                          ".//p[1]",
-                                                          ".//h3/a[@href]/@href" } },
-            //{SiteName.Ria, new string[] { "https://ria.ru/organization_Kronockijj_zapovednik/",
-                                          //".//div[@class='list-item']",
-                                          //".//div[@class='list-item__content']",
-                                          //".//div[@class='list-item__date']",
-                                          //".//div[@class='list-item__content']//a[@href]/@href" } },
-            {SiteName.GoogleNews, new string[] { "https://news.google.com/search?q=%D0%BA%D0%B0%D0%BC%D1%87%D0%B0%D1%82%D0%BA%D0%B0%20%D0%B7%D0%B0%D0%BF%D0%BE%D0%B2%D0%B5%D0%B4%D0%BD%D0%B8%D0%BA%20when%3A7d&hl=ru&gl=RU&ceid=RU%3Aru",
-                                                 ".//div[@class='xrnccd']",
-                                                 ".//h3[@class='ipQwMb ekueJc RD0gLb']",
-                                                 ".//time[@class='WW6dff uQIVzc Sksgp']/@datetime",
-                                                 ".//h3/a[@href]/@href" } },
+            {SiteName.PoluostrovKamchatka, new PageElement() {
+                                          SiteURL = "https://poluostrov-kamchatka.ru",
+                                          RequestURL = "https://poluostrov-kamchatka.ru/pknews/english/",
+                                          ArticlesPath = "//div[@class='article-info']",
+                                          TitlePath = ".//h3",
+                                          TimePath = ".//p[1]",
+                                          LinkPath = ".//h3/a[@href]/@href"  } },
+            //{SiteName.Ria, new PageElement() {
+            //                             SiteURL = "https://ria.ru/organization_Kronockijj_zapovednik/",
+            //                             RequestURL = "https://ria.ru/",
+            //                             ArticlesPath = ".//div[@class='list-item']",
+            //                             TitlePath = ".//div[@class='list-item__content']",
+            //                             TimePath = ".//div[@class='list-item__date']",
+            //                             LinkPath = ".//div[@class='list-item__content']//a[@href]/@href" } },
+            {SiteName.GoogleNews, new PageElement() {
+                                         SiteURL = "https://news.google.com/search?q=%D0%BA%D0%B0%D0%BC%D1%87%D0%B0%D1%82%D0%BA%D0%B0%20%D0%B7%D0%B0%D0%BF%D0%BE%D0%B2%D0%B5%D0%B4%D0%BD%D0%B8%D0%BA%20when%3A7d&hl=ru&gl=RU&ceid=RU%3Aru",
+                                         RequestURL = "https://news.google.com",
+                                         ArticlesPath = ".//div[@class='xrnccd']",
+                                         TitlePath = ".//h3[@class='ipQwMb ekueJc RD0gLb']",
+                                         TimePath = ".//time[@class='WW6dff uQIVzc Sksgp']/@datetime",
+                                         LinkPath = ".//h3/a[@href]/@href" } },
         };
 
         /// <summary>
@@ -49,11 +55,11 @@ namespace WildHackWebApp.BL
 
         private static async Task<List<EcologyEvent>> ParseSiteAsync(SiteName site, HtmlWeb client)
         {
-            HtmlDocument page = client.Load(siteDict[site][0]);
-            string articlesPath = siteDict[site][1];
-            string titlePath = siteDict[site][2];
-            string timePath = siteDict[site][3];
-            string linkPath = siteDict[site][4];
+            HtmlDocument page = client.Load(siteDict[site].RequestURL);
+            string articlesPath = siteDict[site].ArticlesPath;
+            string titlePath = siteDict[site].TitlePath;
+            string timePath = siteDict[site].TimePath;
+            string linkPath = siteDict[site].LinkPath;
 
             var articles = page.DocumentNode.SelectNodes(articlesPath);
 
