8279606a4583a885c7ba20457370ab6c0472c9f6
SteBullet <malzev.step@yandex.ru>
2021-12-04 12:11:48 +0300

    Добавлена проверка даты

--- WildHackWebApp/BL/Date.cs
+++ WildHackWebApp/BL/Date.cs
@@ -8,10 +8,77 @@ namespace WildHackWebApp.BL
     [Serializable]
     public class Date
     {
+        int[] daysInMonths = { -1, 31, -1, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        private int day;
+        private int month;
+        private int year;
         //TODO логику для сета гета
-        public int Day { get; set; }
-        public int Month { get; set; }
-        public int Year { get; set; }
+        public int Day
+        {
+            set
+            {
+                if (value < 1)
+                    day = -1;
+                else
+                {
+                    if (month == 2)
+                    {
+                        if (value < 29)
+                            day = value;
+                        else
+                        {
+                            if (value == 29 && year % 4 == 0)
+                                day = 29;
+                            else
+                                day = -1;
+                        }
+                            
+                    }
+                    else
+                    {
+                        if (value > daysInMonths[month])
+                            day = -1;
+                        else
+                            day = value;
+                    }
+                }
+            }
+            get
+            {
+                return day;
+            }
+        }
+
+        public int Month
+        {
+            set
+            {
+                if (value > 12 || value < 1)
+                    month = -1;
+                else
+                    month = value;
+            }
+            get
+            {
+                return month;
+            }
+        }
+
+        public int Year
+        {
+            set
+            {
+                if (value > DateTime.Now.Year || value < 1)
+                    year = -1;
+                else
+                    year = value;
+            }
+            get
+            {
+                return year;
+            }
+        }
 
         public string FullDate
         {
