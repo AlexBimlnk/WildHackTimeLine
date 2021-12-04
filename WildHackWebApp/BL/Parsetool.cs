6f1649f8189dbe2ca46b929c55a4c4010f877410
Cassie Welsh <maksim.airapetyan3@yandex.ru>
2021-12-04 14:57:01 +0300

    Допилил SiteName и переписал парсер

--- WildHackWebApp/BL/Parsetool.cs
+++ WildHackWebApp/BL/Parsetool.cs
@@ -12,8 +12,21 @@ namespace WildHackWebApp.BL
     {
         private static Dictionary<SiteName, string[]> siteDict = new Dictionary<SiteName, string[]>
         {
-            //{SiteName.SiteName1, new string[] { "https://poluostrov-kamchatka.ru/pknews/english/", "//div[@class='article-info']", "./h3", "./p[1]", ".//h3/a[@href]" } },
-            {SiteName.SiteName2, new string[] { "https://ria.ru/organization_Kronockijj_zapovednik/", "//div[@class='list-item']", ".//div[@class='list-item__content']", ".//div[@class='list-item__date']", ".//div[@class='list-item__content']/a[@href]" } },
+            {SiteName.PoluostrovKamchatka, new string[] { "https://poluostrov-kamchatka.ru/pknews/english/",
+                                                          "//div[@class='article-info']",
+                                                          "//h3",
+                                                          "//p[1]",
+                                                          "//h3/a[@href]/@href" } },
+            {SiteName.Ria, new string[] { "https://ria.ru/organization_Kronockijj_zapovednik/",
+                                          "//div[@class='list-item']",
+                                          "//div[@class='list-item__content']",
+                                          "//div[@class='list-item__date']",
+                                          "//div[@class='list-item__content']//a[@href]/@href" } },
+            //{SiteName.GoogleNews, new string[] { "https://news.google.com/search?q=%D0%BA%D0%B0%D0%BC%D1%87%D0%B0%D1%82%D0%BA%D0%B0%20%D0%B7%D0%B0%D0%BF%D0%BE%D0%B2%D0%B5%D0%B4%D0%BD%D0%B8%D0%BA%20when%3A1d&hl=ru&gl=RU&ceid=RU%3Aru",
+                                                 //"//div[@class='xrnccd']",
+                                                 //"//h3[@class='ipQwMb ekueJc RD0gLb']",
+                                                 //"//time[@class='WW6dff uQIVzc Sksgp']",
+                                                 //"//h3/a[@href]/@href" } },
         };
 
         /// <summary>
@@ -24,7 +37,8 @@ namespace WildHackWebApp.BL
         {
             List<EcologyEvent> resultList = new List<EcologyEvent>();
             HtmlWeb client = new HtmlWeb();
-            client.UserAgent = " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36";
+
+            client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36";
             foreach(var site in siteDict.Keys)
             {
                 resultList.AddRange(await ParseSiteAsync(site, client));
@@ -47,9 +61,10 @@ namespace WildHackWebApp.BL
             foreach (var article in articles)
             {
                 EcologyEvent ecoEvent = new EcologyEvent();
-                ecoEvent.Title = article.SelectSingleNode(titlePath).InnerText;
-                ecoEvent.Date = new Date(article.SelectSingleNode(timePath).InnerText);
-                ecoEvent.Link = article.SelectSingleNode(linkPath).Attributes["href"].Value;
+                HtmlNodeNavigator navigator = (HtmlNodeNavigator) article.CreateNavigator();
+                ecoEvent.Title = navigator.SelectSingleNode(titlePath).Value;
+                ecoEvent.Date = new Date(navigator.SelectSingleNode(timePath).Value);
+                ecoEvent.Link = navigator.SelectSingleNode(linkPath).Value;
                 resultList.Add(ecoEvent);
             }
 
